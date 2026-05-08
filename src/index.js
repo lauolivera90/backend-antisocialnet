c41874ebd7179cea8d98e6ec66bb0c652ba369a8
lauolivera90 <lautarobolivera098@gmail.com>
2026-05-08 00:20:33 -0300

    add deployed front path on cors

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ const app = express()
 const PORT = process.env.PORT || 3000
 
 app.use(express.json())
-app.use(cors({ origin: 'http://localhost:5173' }));
+app.use(cors({ origin: ['http://localhost:5173', 'https://anti-social-net.vercel.app'] }));
 
 
 const swaggerDocument = YAML.load('./docs/swagger.yaml');
